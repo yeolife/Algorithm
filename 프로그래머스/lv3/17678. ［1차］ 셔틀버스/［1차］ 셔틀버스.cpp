72ea6bc116eb6eb9840992a0789de62ea8518dd8
여창민(ChangMin Yeo) <82012857+yeolife@users.noreply.github.com>
2023-10-05 04:52:22 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 0.74 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
+++ 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.cpp
@@ -4,9 +4,8 @@ using namespace std;
 
 vector<int> timestamp;
 
+// 편의상 시간을 정수로 변환
 void StoI(vector<string> timetable) {
-    // 편의상 시간을 정수로 변경
-    
     for(int i = 0; i < timetable.size(); i++) {
         bool flag = false;
         stringstream ss;
@@ -24,6 +23,7 @@ void StoI(vector<string> timetable) {
     }
 }
 
+// 시간을 문자열로 재변환
 string TtoS(int t) {
     string h = to_string(t / 60);
     string m = to_string(t % 60);
@@ -38,53 +38,52 @@ string TtoS(int t) {
         reverse(m.begin(), m.end());
     }
     
-    
     string ret = h + ":" + m;
     
     return ret;
 }
 
 string solution(int n, int t, int m, vector<string> timetable) {
-    int ans = 0;
+    int ans = 540;
     string answer = "";
     
+    stack<int> st;
     priority_queue<int, vector<int>, greater<int> > pq;
     
     StoI(timetable);
     
     for(auto& it: timestamp)
         pq.push(it);
-    
-    int cur = 9 * 60;
-    for(int i = 0; i < n; i++) {
-        int cnt = 0;
-        
-        if(pq.empty() || (!pq.empty() && pq.top() > 9 * 60 + t * (n-1))) {
-            ans = cur + t*(n-1-i);
+
+    for(int i = 0, cur = 9 * 60; i < n; i++, cur += t) {
+        // 크루원 신경 안 쓰고 제일 늦게 가도 널널함
+        if(pq.empty() || (pq.top() > 9 * 60 + t * (n-1))) {
+            ans = cur + t * (n-1-i);
             break;
         }
         
-        stack<int> st;
+        // 현재 셔틀에 최대한 태움
+        int cnt = 0;
         while(!pq.empty() && cur >= pq.top() && cnt < m) {
-            st.push(pq.top());
-            pq.pop();
+            if(i == n-1)
+                st.push(pq.top());
+            
             cnt++;
+            pq.pop();
         }
         
-        if(cnt < m)
-            ans = cur;
-        else {
-            if(!st.empty()) {
-                int dup = st.top(); st.pop();
-                while(!st.empty() && dup == st.top()) {
-                    dup = st.top();
-                    st.pop();
-                }
-                ans = dup - 1;
-            }
+        if(cnt < m) ans = cur;
+    }
+    
+    // 현재 셔틀이 꽉 차면 
+    if(m == st.size()) {
+        // 가장 늦은 시각을 중복을 확인하며 내리게 함
+        int dup = st.top(); st.pop();
+        while(!st.empty() && dup == st.top()) {
+            dup = st.top();
+            st.pop();
         }
-            
-        cur += t;
+        ans = dup - 1;
     }
     
     answer = TtoS(ans);
@@ -94,5 +93,3 @@ string solution(int n, int t, int m, vector<string> timetable) {
 
 // 최대한 늦게 도착하지만 꼭 타야함.
 // 중복 예외를 처리해야함.
-
-
