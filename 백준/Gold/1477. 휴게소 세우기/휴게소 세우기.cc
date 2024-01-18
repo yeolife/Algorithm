779fac544a756d7ba880d589083dc1158558530e
여창민 <82012857+yeolife@users.noreply.github.com>
2024-01-18 20:49:00 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+#define x first
+#define y second
+
+int n, m, l, ans = 1000;
+int arr[53];
+
+bool pram(int mid) {
+  if (mid == 0)
+    return true;
+
+  int cnt = 0;
+
+  int tmp = -1;
+  for (int i = 1; i < n; i++) {
+    if (arr[i] - arr[i - 1] <= mid || arr[i] - arr[i - 1] == 1)
+      continue;
+
+    if ((arr[i] - arr[i - 1]) / mid > 1)
+      tmp = max(tmp, mid + (arr[i] - arr[i - 1]) % mid);
+
+    cnt += (arr[i] - arr[i - 1]) / mid;
+    if ((arr[i] - arr[i - 1]) % mid == 0)
+      cnt--;
+  }
+
+  if (cnt >= m) {
+    if (cnt == m)
+      return false;
+    return true;
+  }
+  return false;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(NULL);
+
+  cin >> n >> m >> l;
+
+  int tmp[53];
+  for (int i = 0; i < n; i++)
+    cin >> tmp[i];
+
+  sort(tmp, tmp + n);
+
+  int idx = 0;
+  if (tmp[0] > 1) {
+    arr[0] = 0;
+    idx++;
+  }
+
+  for (int i = 0; i < n; i++)
+    arr[i + idx] = tmp[i];
+
+  if (arr[n - 1 + idx] < l) {
+    arr[n + idx] = l;
+    idx++;
+  }
+
+  n += idx;
+
+  int lo = 0, hi = 1e6;
+  while (lo + 1 < hi) {
+    int mid = (lo + hi) / 2;
+
+    if (pram(mid))
+      lo = mid;
+    else
+      hi = mid;
+  }
+
+    cout << hi;
+
+  return 0;
+}
