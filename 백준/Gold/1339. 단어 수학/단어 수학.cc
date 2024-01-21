764958a5cfc5d1fc7c4a0a18ea096eed5a8119df
여창민 <82012857+yeolife@users.noreply.github.com>
2024-01-21 20:10:16 +0900

    [Gold IV] Title: 단어 수학, Time: 1780 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.cc
+++ 백준/Gold/1339. 단어 수학/단어 수학.cc
@@ -4,7 +4,7 @@ using namespace std;
 int n, ans;
 int alpha_cnt;
 map<char, int> mp;
-vector<int> vec;
+vector<int> vec(10);
 string strs[10];
 bool visited[10];
 
@@ -12,7 +12,7 @@ bool cmp(string a, string b) {
     return a.length() > b.length();
 }
 
-void dfs(int depth, int idx) {
+void dfs(int depth) {
     if(depth >= alpha_cnt) {
         int sum = 0;
         for(int i = 0; i < n; i++) {
@@ -30,15 +30,14 @@ void dfs(int depth, int idx) {
     }
 
     for(int i = 9; i > 9 - alpha_cnt; i--) {
-        if(visited[i]) continue;
+        if(visited[i]) continue; // 중복되는 알파벳이면 패스
 
         visited[i] = true;
-        vec.push_back(i);
+        vec[depth] = i;
 
-        dfs(depth + 1, i - 1);
+        dfs(depth + 1);
 
         visited[i] = false;
-        vec.pop_back();
     }
 }
 
@@ -52,6 +51,7 @@ int main() {
 
     sort(strs, strs+n, cmp);
 
+    // 9부터 알파벳의 개수만큼 순열을 돌림
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < strs[i].length(); j++) {
             if(mp.count(strs[i][j]) == 0) {
@@ -61,9 +61,17 @@ int main() {
         }
     }
 
-    dfs(0, 9);
+    dfs(0);
 
     cout << ans;
 
     return 0;
 }
+
+// 벡터에 알파벳의 종류를 넣는다
+// 중복되지 않고, 이전것을 참조하지 않으면서 벡터에 순열을 만든다.
+// 만약에 모든 순열이 완성되면, 
+
+// 알파벳 -> 순번
+// 알파벳은 크기가 되어야함. (ex A = 9, B = 7)
+// 벡터에서는 순서에 따라 크기를 부여할 예정.(1번째 숫자는 9, 2번째는 8..)
