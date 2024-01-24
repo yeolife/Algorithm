52dade0b073c7d1c8af9d37dbbd950dd0b7d9e6a
여창민 <82012857+yeolife@users.noreply.github.com>
2024-01-24 23:53:33 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int arr[9];
+bool visited[9];
+
+void dfs(int cnt) {
+    if(cnt==m) {
+        for(int i=0; i<m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i=1; i<=n; i++) {
+        if(!visited[i]) {
+            arr[cnt]=i;
+            visited[i]=true;
+            dfs(cnt+1);
+            visited[i]=false;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+    dfs(0);
+}
