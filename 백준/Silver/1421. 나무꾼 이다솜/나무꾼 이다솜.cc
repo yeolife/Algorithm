d0f8a0d6687288b73b54900fe3ff9a4d8ce919bf
여창민 <82012857+yeolife@users.noreply.github.com>
2024-01-25 22:49:02 +0900

    [Silver I] Title: 나무꾼 이다솜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.cc
+++ 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.cc
@@ -1,46 +1,53 @@
-#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
-long long n, c, w;
-long long trees[51];
+int n, c, w;
+int trees[51];
+int max_val = -987654321;
 
-long long dfs(long long tree) {
-    long long cnt = 0, cut = 0, ret = 0;
-
+long long solve(int tree_len) {
+    int tree_cnt = 0;
+    int tree_cut_cnt = 0;
+    long long temp_ans = 0;
     for (int i = 0; i < n; i++) {
-        if (trees[i] % tree > 0)
-                cut = trees[i] / tree;
-            else 
-                cut = trees[i] / tree - 1;
+        
+        if (trees[i] == tree_len)
+            tree_cnt++;
+            
+        if (trees[i] % tree_len == 0)
+            tree_cut_cnt = trees[i] / tree_len - 1;
+        else 
+            tree_cut_cnt = trees[i] / tree_len;
 
-        cnt = trees[i] / tree;
+        tree_cnt = trees[i] / tree_len;
+       
+        long long temp = tree_cnt * tree_len * w - c * tree_cut_cnt;
 
-        long long good = (tree * cnt * w) - (cut * c);
-
-        if(good > 0)
-            ret += good;
+        if (temp > 0)
+            temp_ans += temp;
     }
 
-    return ret;
+    return temp_ans;
 }
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(NULL);
 
+int main() {
     cin >> n >> c >> w;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         cin >> trees[i];
+        if (max_val < trees[i]) max_val = trees[i];
+    }
 
-    sort(trees, trees + n);
+    long long ans = -987654321;
+    for (int tree_len = 1; tree_len <= max_val; tree_len++) {
+        long long ret = solve(tree_len);
+        if (ret > ans) ans = ret;
+    }
 
-    long long ans = trees[n - 1] * w;
-
-    for (int i = 1; i <= 10000; i++)
-        ans = max(ans, dfs(i));
-
-    cout << ans;
-
+    printf("%lld\n", ans);
     return 0;
 }
