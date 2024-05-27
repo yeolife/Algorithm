e11b982e878b340526874efbc8e312b53c785d22
여창민 <82012857+yeolife@users.noreply.github.com>
2024-05-27 22:13:46 +0900

    [level 3] Title: 상담원 인원, Time: 10.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/214288. 상담원 인원/상담원 인원.cpp
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 using pii = pair<int, int>; 
 
-int n, k, ans = 1e9, mento[5];
+int k, ans = 1e9, mento[5];
 vector<pii> req[5];
 
 int oper_time(int n) {
@@ -13,14 +13,14 @@ int oper_time(int n) {
     for(; i < mento[n] && i < req[n].size(); i++)
         pq.push(req[n][i].first + req[n][i].second);
         
-    // 도착시간, 상담시간, 순번
+    
     while(i < req[n].size() && !pq.empty()) {
+        // 도착시간, 상담시간
         int dt, st;
-        tie(dt, st) = req[n][i];
+        tie(dt, st) = req[n][i++];
         
         int cur = pq.top();
         pq.pop();
-        i++;
         
         if(dt < cur) {
             ret += cur - dt;
@@ -34,7 +34,6 @@ int oper_time(int n) {
 }
 
 void dfs(int depth, int remain) {
-    if(depth > k) return;
     if(remain == 0) {
         int ret = 0;
         for(int i = 0; i < k; i++) {
@@ -46,6 +45,7 @@ void dfs(int depth, int remain) {
         
         return;
     }
+    if(depth >= k) return;
     
     // (n-k)명의 멘토를 조합
     for(int i = 0; i <= remain; i++) {
@@ -56,13 +56,13 @@ void dfs(int depth, int remain) {
 }
 
 // 가장 적게 기다린 총 시간
-int solution(int kk, int nn, vector<vector<int>> reqs) {
-    n = nn, k = kk;
+int solution(int kk, int n, vector<vector<int>> reqs) {
+    k = kk;
     
     for(int i = 0; i < reqs.size(); i++)
         req[reqs[i][2] - 1].push_back({reqs[i][0], reqs[i][1]});
     
-    fill(mento, mento + 5, 1);
+    fill(mento, mento + k, 1);
     
     dfs(0, n - k);
     
