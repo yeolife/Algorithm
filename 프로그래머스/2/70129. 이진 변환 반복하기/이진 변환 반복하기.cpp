48fab712442a8b04d833271797710d2021dcf4fc
여창민 <82012857+yeolife@users.noreply.github.com>
2024-06-16 14:24:36 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.04 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -5,22 +5,14 @@ vector<int> solution(string s) {
     vector<int> answer(2, 0);
     
     while(s != "1") {
-        // 1의 갯수를 셈
-        int zero = 0, one = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(s[i] == '1') one++;
-            else zero++;
-        }
-        
-        s = "";
-        
-        while(one) {
-            s += (one%2) + '0';
-            one /= 2;
-        }
+        int one = count(s.begin(), s.end(), '1');
+        int zero = s.length() - one;
         
         answer[0]++;
         answer[1] += zero;
+        
+	    s = bitset<150000>(one).to_string();
+        s = s.substr(s.find('1'));
     }
     
     return answer;
